b66a6cf7b053d22453ecba01baf393979fddd7b2
shinbunbun <takamuneyuto@gmail.com>
2023-09-16 18:05:03 +0900

    impl GetAnswersByUserID

--- server/model/answer.go
+++ server/model/answer.go
@@ -16,3 +16,9 @@ func (Answer) GetAnswers(db *gorm.DB) []Answer {
 	db.Find(&answers)
 	return answers
 }
+
+func (Answer) GetAnswersByUserID(db *gorm.DB, userID string) []Answer {
+	var answers []Answer
+	db.Where("user_id = ?", userID).Find(&answers)
+	return answers
+}
