7ebb985cd8eebe3f93cc3462e2ae36ee7c41f909
shinbunbun <takamuneyuto@gmail.com>
2023-09-16 18:04:17 +0900

    impl GetAnswers

--- server/model/answer.go
+++ server/model/answer.go
@@ -1,8 +1,18 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type Answer struct {
 	ID      string `gorm:"primary_key"`
 	Answer  string
 	UserID  string
 	ThemeID string
+}
+
+func (Answer) GetAnswers(db *gorm.DB) []Answer {
+	var answers []Answer
+	db.Find(&answers)
+	return answers
 }
