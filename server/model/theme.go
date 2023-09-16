3cbd186ead473d58c8d2a3b121b1a10be5eec4aa
shinbunbun <takamuneyuto@gmail.com>
2023-09-16 18:07:37 +0900

    impl GetThemeByThemeID

--- server/model/theme.go
+++ server/model/theme.go
@@ -1,6 +1,13 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Theme struct {
 	ID   string `gorm:"primary_key"`
 	Name string
+}
+
+func (Theme) GetThemeByThemeID(db *gorm.DB, themeID string) (theme Theme, err error) {
+	err = db.Where("id = ?", themeID).First(&theme).Error
+	return
 }
