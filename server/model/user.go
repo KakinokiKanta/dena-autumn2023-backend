b85cb5525cdef4f4eb5f16a675d1ed5b25b937e5
shinbunbun <takamuneyuto@gmail.com>
2023-09-16 18:07:07 +0900

    impl GetUserByUserID & PutUser

--- server/model/user.go
+++ server/model/user.go
@@ -1,7 +1,18 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type User struct {
 	ID    string `gorm:"primary_key"`
 	Name  string
 	IsNew bool
+}
+
+func (User) GetUserByUserID(db *gorm.DB, userID string) (user User, err error) {
+	err = db.Where("id = ?", userID).First(&user).Error
+	return
+}
+
+func (User) PutUser(db *gorm.DB, user User) {
+	db.Create(&user)
 }
